1800192d58578199af4d1648f70bc5d162936aaf
JoshPattman <Josh.pattman@gmail.com>
2025-05-08 21:36:19 +0100

    Add embedding model support (openai)

---
+++ embedder.go
@@ -0,0 +1,30 @@
+package jpf
+
+import (
+	"errors"
+	"math"
+)
+
+type Embedder interface {
+	Embed(text string) ([]float64, error)
+}
+
+func CosineSimilarity(vec1, vec2 []float64) (float64, error) {
+	if len(vec1) != len(vec2) {
+		return 0, errors.New("vectors must be of the same length")
+	}
+
+	var dotProduct, magnitudeVec1, magnitudeVec2 float64
+
+	for i := range vec1 {
+		dotProduct += vec1[i] * vec2[i]
+		magnitudeVec1 += vec1[i] * vec1[i]
+		magnitudeVec2 += vec2[i] * vec2[i]
+	}
+
+	if magnitudeVec1 == 0 || magnitudeVec2 == 0 {
+		return 0, errors.New("one of the vectors has zero magnitude")
+	}
+
+	return dotProduct / (math.Sqrt(magnitudeVec1) * math.Sqrt(magnitudeVec2)), nil
+}
---
+++ embedder_openai.go
@@ -0,0 +1,57 @@
+package jpf
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+)
+
+func NewOpenAIEmbedder(key string, model string) Embedder {
+	return &openAIEmbedder{
+		key:   key,
+		model: model,
+	}
+}
+
+type openAIEmbedder struct {
+	key   string
+	model string
+}
+
+func (o *openAIEmbedder) Embed(text string) ([]float64, error) {
+	bodyMap := map[string]any{
+		"input": text,
+		"model": o.model,
+	}
+	body, err := json.Marshal(bodyMap)
+	if err != nil {
+		return nil, err
+	}
+	req, err := http.NewRequest("POST", "https://api.openai.com/v1/embeddings", bytes.NewBuffer(body))
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Add("Authorization", fmt.Sprintf("Bearer %s", o.key))
+	req.Header.Add("Content-Type", "application/json")
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	respBody, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+	respTyped := struct {
+		Data []struct {
+			Embedding []float64 `json:"embedding"`
+		} `json:"data"`
+	}{}
+	err = json.Unmarshal(respBody, &respTyped)
+	if err != nil || len(respTyped.Data) != 1 || len(respTyped.Data[0].Embedding) == 0 {
+		return nil, fmt.Errorf("failed to parse response (%v): %s", err.Error(), string(respBody))
+	}
+	return respTyped.Data[0].Embedding, nil
+}
