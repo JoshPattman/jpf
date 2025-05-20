226d9a857d509ae8bf9e785ca1001094e3b0281d
Josh Pattman <Josh.pattman@gmail.com>
2025-05-20 23:26:49 +0100

    Further simplify the agent

--- agent.go
+++ agent.go
@@ -5,53 +5,58 @@ import (
 	"iter"
 )
 
-// Action[state] defines an action that an agent has produced.
-// An action is capable of updating the agent's state.
-type Action[T any] interface {
-	// DoAction updates the agent state, returning the new state, if it was a terminal action
-	DoAction(state T) (T, bool, error)
+// Action defines an action that an agent has produced.
+// An action may do anything, but usually it updates the agent's state.
+type Action interface {
+	// DoAction runs the action, updates the agent state.
+	DoAction() error
 }
 
-// Agent[state] defines some agentic behaviour.
-// It does not contain any state or models itself, only configuration.
-// Abstractly, an agent is somthing that can pick a next action from the given state.
-type Agent[T any] interface {
-	// Function is the functin that is called to generate the next action of the agent.
-	Function[T, Action[T]]
+// Agent defines a stateful agent, capable of generating next actions with no inputs.
+type Agent interface {
+	// BuildInputMessages builds the input messages to be sent to the LLM, given the agents current state.
+	BuildInputMessages() ([]Message, error)
+	// ParseResponseText converts the raw text output of an agent step into the next action.
+	ParseResponseText(string) (Action, error)
 }
 
-// AgentStep[state, action] defines a step the agent has taken this iteration.
-type AgentStep[T any] struct {
-	// State is the newest state of the agent (after taking the action).
-	State T
+var _ Function[Agent, Action] = &agentFunction{}
+
+// agentFunction wraps an agent so we can call a single step as if it were a function.
+type agentFunction struct {
+	Agent
+}
+
+func (f *agentFunction) BuildInputMessages(a Agent) ([]Message, error) {
+	return a.BuildInputMessages()
+}
+
+// AgentStep defines a step the agent has taken this iteration.
+type AgentStep struct {
 	// Action is the most recent action to be taken.
-	Action Action[T]
+	Action Action
 	// Error is only populated when somthing unrecoverable happened (stopping iteration).
 	Error error
 }
 
 // RunAgent will run the agent indefinitely with the model, starting with the inital state.
-func RunAgent[T any](model Model, agent Agent[T], initialState T) iter.Seq[AgentStep[T]] {
-	state := initialState
-	return func(yield func(AgentStep[T]) bool) {
+func RunAgent[T any](model Model, agent Agent) iter.Seq[AgentStep] {
+	agentfn := &agentFunction{agent}
+	return func(yield func(AgentStep) bool) {
 		for {
-			nextAction, _, err := RunOneShot(model, agent, state)
+			nextAction, _, err := RunOneShot(model, agentfn, agent)
 			if err != nil {
-				yield(AgentStep[T]{*new(T), nil, errors.Join(errors.New("failed to get next action"), err)})
+				yield(AgentStep{nil, errors.Join(errors.New("failed to get next action"), err)})
 				return
 			}
-			nextState, terminal, err := nextAction.DoAction(state)
+			err = nextAction.DoAction()
 			if err != nil {
-				yield(AgentStep[T]{*new(T), nil, errors.Join(errors.New("failed to apply next action"), err)})
+				yield(AgentStep{nil, errors.Join(errors.New("failed to apply next action"), err)})
 				return
 			}
-			if !yield(AgentStep[T]{nextState, nextAction, nil}) {
+			if !yield(AgentStep{nextAction, nil}) {
 				return
 			}
-			if terminal {
-				return
-			}
-			state = nextState
 		}
 	}
 }
