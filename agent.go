174a42bac4bbd839c858bd6f8295504898237a95
Josh Pattman <Josh.pattman@gmail.com>
2025-05-20 22:07:33 +0100

    Make it so actions run themselves to avoid type switching in agent

--- agent.go
+++ agent.go
@@ -5,43 +5,47 @@ import (
 	"iter"
 )
 
-// Agent[state, action] defines some agentic behaviour.
+// Action[state] defines an action that an agent has produced.
+// An action is capable of updating the agent's state.
+type Action[T any] interface {
+	// DoAction updates the agent state, returning the new state, if it was a terminal action
+	DoAction(state T) (T, bool, error)
+}
+
+// Agent[state] defines some agentic behaviour.
 // It does not contain any state or models itself, only configuration.
-// Abstractly, an agent is somthing that can pick a next action from the given state, then apply that action to its state.
-type Agent[T, U any] interface {
+// Abstractly, an agent is somthing that can pick a next action from the given state.
+type Agent[T any] interface {
 	// Function is the functin that is called to generate the next action of the agent.
-	Function[T, U]
-	// Handle integrates the given action into the state.
-	// It returns a new state, a boolean that is tru if that action was terminal, and a terminal error (if any).
-	Handle(T, U) (T, bool, error)
+	Function[T, Action[T]]
 }
 
 // AgentStep[state, action] defines a step the agent has taken this iteration.
-type AgentStep[T, U any] struct {
+type AgentStep[T any] struct {
 	// State is the newest state of the agent (after taking the action).
 	State T
 	// Action is the most recent action to be taken.
-	Action U
+	Action Action[T]
 	// Error is only populated when somthing unrecoverable happened (stopping iteration).
 	Error error
 }
 
-// RunAgent[state, action] will run the agent indefinitely with the model, starting with the inital state.
-func RunAgent[T, U any](model Model, agent Agent[T, U], initialState T) iter.Seq[AgentStep[T, U]] {
+// RunAgent will run the agent indefinitely with the model, starting with the inital state.
+func RunAgent[T any](model Model, agent Agent[T], initialState T) iter.Seq[AgentStep[T]] {
 	state := initialState
-	return func(yield func(AgentStep[T, U]) bool) {
+	return func(yield func(AgentStep[T]) bool) {
 		for {
 			nextAction, _, err := RunOneShot(model, agent, state)
 			if err != nil {
-				yield(AgentStep[T, U]{*new(T), *new(U), errors.Join(errors.New("failed to get next action"), err)})
+				yield(AgentStep[T]{*new(T), nil, errors.Join(errors.New("failed to get next action"), err)})
 				return
 			}
-			nextState, terminal, err := agent.Handle(state, nextAction)
+			nextState, terminal, err := nextAction.DoAction(state)
 			if err != nil {
-				yield(AgentStep[T, U]{*new(T), *new(U), errors.Join(errors.New("failed to apply next action"), err)})
+				yield(AgentStep[T]{*new(T), nil, errors.Join(errors.New("failed to apply next action"), err)})
 				return
 			}
-			if !yield(AgentStep[T, U]{nextState, nextAction, nil}) {
+			if !yield(AgentStep[T]{nextState, nextAction, nil}) {
 				return
 			}
 			if terminal {
