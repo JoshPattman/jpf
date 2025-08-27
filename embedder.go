dec4e46dc3bb9e39baf3b5ffb68ec124f9e56ecb
josh.pattman <josh.pattman@luminance.com>
2025-08-27 09:33:50 +0100

    Change the embedder constructor too

--- embedder.go
+++ embedder.go
@@ -5,6 +5,10 @@ import (
 	"math"
 )
 
+type EmbedderBuilder interface {
+	New() (Embedder, error)
+}
+
 // Embedder defines an object that is capable of embedding a string into a vector.
 type Embedder interface {
 	Embed(text string) ([]float64, error)
