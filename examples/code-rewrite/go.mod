592afb1d79a4c17bfcad0fe4ec39509d86c445b4
JoshPattman <Josh.pattman@gmail.com>
2025-09-01 23:06:14 +0100

    Add example

---
+++ examples/code-rewrite/go.mod
@@ -0,0 +1,3 @@
+module code-rewrite
+
+go 1.23.1
---
+++ examples/code-rewrite/main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	_ "embed"
+	"flag"
+	"fmt"
+	"io"
+	"os"
+	"strings"
+
+	"github.com/JoshPattman/jpf"
+)
+
+//go:embed system.md
+var system string
+
+type TemplateData struct {
+	Language string
+	Pointers []string
+	Code     string
+}
+
+func main() {
+	inputFile := flag.String("i", "", "The input file to use")
+	targetLang := flag.String("l", "Python", "Target language for the rewrite")
+	pointers := flag.String("p", "You may change the overall structure;Make sure to keep the functionality the same", "Semicolon separated list of pointers")
+	flag.Parse()
+
+	model := jpf.NewOpenAIModel(os.Getenv("OPENAI_KEY"), "gpt-4o-mini")
+	formatter := jpf.NewTemplateMessageEncoder[TemplateData](system, "{{.Code}}")
+	parser := jpf.NewRawStringResponseDecoder()
+	mapFunc := jpf.NewOneShotMapFunc(formatter, parser, model)
+
+	f, err := os.Open(*inputFile)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer f.Close()
+
+	data, err := io.ReadAll(f)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	rewritten, _, err := mapFunc.Call(TemplateData{
+		Language: *targetLang,
+		Pointers: strings.Split(*pointers, ";"),
+		Code:     string(data),
+	})
+	fmt.Println(rewritten)
+}
