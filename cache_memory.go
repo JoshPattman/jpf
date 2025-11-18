0014317876e7cb9f05a2461081ddbac10429e3ae
Josh Pattman <josh.pattman@gmail.com>
2025-11-18 23:27:20 +0000

    Add a cache that persists to disk but lives in memory

--- cache_memory.go
+++ cache_memory.go
@@ -6,24 +6,24 @@ import "context"
 // It stores model responses in memory using a hash of the input messages as a key.
 func NewInMemoryCache() ModelResponseCache {
 	return &inMemoryCache{
-		resps: make(map[string]memoryCachePacket),
+		Resps: make(map[string]memoryCachePacket),
 	}
 }
 
 type memoryCachePacket struct {
-	aux   []Message
-	final Message
+	Aux   []Message
+	Final Message
 }
 
 type inMemoryCache struct {
-	resps map[string]memoryCachePacket
+	Resps map[string]memoryCachePacket
 }
 
 // GetCachedResponse implements ModelResponseCache.
 func (i *inMemoryCache) GetCachedResponse(ctx context.Context, salt string, msgs []Message) (bool, []Message, Message, error) {
 	msgsHash := HashMessages(salt, msgs)
-	if cp, ok := i.resps[msgsHash]; ok {
-		return true, cp.aux, cp.final, nil
+	if cp, ok := i.Resps[msgsHash]; ok {
+		return true, cp.Aux, cp.Final, nil
 	}
 	return false, nil, Message{}, nil
 }
@@ -31,9 +31,9 @@ func (i *inMemoryCache) GetCachedResponse(ctx context.Context, salt string, msgs
 // SetCachedResponse implements ModelResponseCache.
 func (i *inMemoryCache) SetCachedResponse(ctx context.Context, salt string, inputs []Message, aux []Message, out Message) error {
 	msgsHash := HashMessages(salt, inputs)
-	i.resps[msgsHash] = memoryCachePacket{
-		aux:   aux,
-		final: out,
+	i.Resps[msgsHash] = memoryCachePacket{
+		Aux:   aux,
+		Final: out,
 	}
 	return nil
 }
